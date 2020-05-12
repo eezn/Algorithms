a15cfed8bad8cca52e27da395f582670be7e3279
eezn <55613344+eezn@users.noreply.github.com>
2020-05-13 03:07:58 +0900

    단순 문자열 압축

---
+++ 1, 2/02 내장 시퀀스 타입/2.8_simple_str_compression.py
@@ -0,0 +1,23 @@
+# 단순 문자열 압축
+# aabcccccaaa -> a2b1c5a3
+
+
+def str_compression(s):
+    count, last = 1, ""
+    list_aux = []
+    for i, c in enumerate(s):
+        if last == c:
+            count += 1
+        else:
+            if i != 0:
+                list_aux.append(str(count))
+            list_aux.append(c)
+            count = 1
+            last = c
+    list_aux.append(str(count))
+    return "".join(list_aux)
+
+
+if __name__ == "__main__":
+    result = str_compression("aabcccccaaa")
+    print(result)
